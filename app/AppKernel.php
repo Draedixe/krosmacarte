6a947d39e6de964429addfb7d3f3db7a52938d27
amargottin <a.margottin@cat-amania.com>
2017-01-11 16:26:03 +0100

    Ajout supression Extension REPARATION

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new CarteBundle\CarteBundle(),
         );
